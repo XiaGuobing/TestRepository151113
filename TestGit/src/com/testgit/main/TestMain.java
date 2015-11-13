b7b7bb46b1b0beb71865f1009dd75b8ff716342b
xiagb <x.yuanxin@qq.com>
2015-11-13 15:54:13 +0800

    添加了一个测试类

---
+++ TestGit/src/com/testgit/main/TestMain.java
@@ -0,0 +1,14 @@
+package com.testgit.main;
+
+/**
+ * ≤‚ ‘¿‡
+ * @author XiaGuobing
+ * 2015-11-13
+ */
+public class TestMain {
+	
+	public static void mian(String[] args){
+		System.out.println("Hello,Wolrd!");
+	}
+	
+}
