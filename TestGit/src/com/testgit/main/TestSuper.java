25eb50624bfeee2edd2c6f5aa4958b52b0746db1
xiagb <x.yuanxin@qq.com>
2015-11-13 16:19:00 +0800

    create a new java class!

---
+++ TestGit/src/com/testgit/main/TestSuper.java
@@ -0,0 +1,19 @@
+package com.testgit.main;
+
+/**
+ * 测试类
+ * @author XiaGuobing
+ * 2015-11-13
+ */
+public class TestSuper {
+	
+	public static void mian(String[] args){
+		// 初始化
+		System.out.println("Hello,Wolrd!");
+		// 添加新的内容
+		System.out.println("add new content!");
+		// 再次添加新内容
+		System.out.println("again add new content!");
+	}
+	
+}
